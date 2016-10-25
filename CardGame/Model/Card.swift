03c3fc2640175db8b677e895c7d646deef4dd099
Pbra1484 <pbra1484@740s-pl-108.local>
2016-10-25 13:27:15 -0600

    added meters in card class

--- CardGame/Model/Card.swift
+++ CardGame/Model/Card.swift
@@ -18,4 +18,15 @@ class Card
         backImage = UIImage()
         isFaceUp = false
     }
+    
+    func getBackImage() -> UIImage{return backImage}
+    
+    func isFacing() -> Bool{return isFaceUp}
+    
+    func toString() ->String
+    {
+        let description = "This card is \(isFaceUp)"
+        
+        return description
+    }
 }
