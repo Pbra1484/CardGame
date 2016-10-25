d1aef19230032da862a3b4c9d7b573196d962dc0
Pbra1484 <pbra1484@740s-pl-108.local>
2016-10-25 13:42:46 -0600

    Added playing card class methods

--- CardGame/Model/PlayingCard.swift
+++ CardGame/Model/PlayingCard.swift
@@ -6,4 +6,44 @@
 //  Copyright © 2016 Brashear, Patrick. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PlayingCard : Card
+{
+    private var rank : Int
+    private var suit : String
+    private var color : UIColor
+    
+    override init()
+    {
+        rank = 0
+        suit = ""
+        color = UIColor()
+        super.init()
+    }
+    
+    func getRank() -> Int {return rank}
+    func getSuit() ->String {return suit}
+    func getColor() ->UIColor {return color}
+    
+    override func toString() -> String
+    {
+        let description = "The rank is \(rank) the suit is \(suit) and the color is \(color)"
+        
+        return description
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
