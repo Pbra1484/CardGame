bd323707c231abd0aa4576bc69e407f0988b74b8
Pbra1484 <pbra1484@740s-pl-108.local>
2016-10-31 12:33:54 -0600

    Added deck and subclass playing card deck

---
+++ CardGame/Model/PlayingCardDeck.swift
@@ -0,0 +1,14 @@
+//
+//  PlayingCardDeck.swift
+//  CardGame
+//
+//  Created by Brashear, Patrick on 10/31/16.
+//  Copyright © 2016 Brashear, Patrick. All rights reserved.
+//
+
+import Foundation
+
+class PlayingCardDeck : Deck
+{
+    
+}
