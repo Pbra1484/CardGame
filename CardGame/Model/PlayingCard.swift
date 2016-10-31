f08c64453d03d2cdb1742019d9dd5250842dd5d3
Pbra1484 <pbra1484@740s-pl-108.local>
2016-10-31 12:46:42 -0600

    Changed visibility of all private datamembers to internal

--- CardGame/Model/PlayingCard.swift
+++ CardGame/Model/PlayingCard.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class PlayingCard : Card
 {
-    private var rank : Int
+    internal var rank : Int
     {
         get
         {
@@ -21,7 +21,7 @@ class PlayingCard : Card
             self.rank = rank
         }
     }
-    private var suit : String
+    internal var suit : String
     {
         get
         {
@@ -32,7 +32,7 @@ class PlayingCard : Card
             self.suit = suit
         }
     }
-    private var color : UIColor
+    internal var color : UIColor
     {
         get
         {
