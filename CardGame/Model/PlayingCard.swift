5cc57a0af9beb8ecfe757a4dc3206c1ab8436799
Pbra1484 <pbra1484@740s-pl-108.local>
2016-10-31 12:59:22 -0600

    added class fun ranks and suits

--- CardGame/Model/PlayingCard.swift
+++ CardGame/Model/PlayingCard.swift
@@ -75,8 +75,23 @@ class PlayingCard : Card
         return description
     }
     
+    //The class modifier mke is so the method is visible without an instace 
+    //You would calll it by ClassName.method()
+    //In this cas PlayingCard.CalidRanks()
+    class func validRanks() -> [String]
+    {
+        return ["??","A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    }
     
+    class func maxRank() -> Int
+    {
+        return validRanks().count-1
+    }
     
+    class func validSuits() -> [String]
+    {
+        return ["♦️","♥️","♣️","♠️"]
+    }
     
     
     
