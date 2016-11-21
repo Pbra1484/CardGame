58861ee0c3dcc97b5971d30b4e64a96b280cdbbe
Pbra1484 <pbra1484@740s-pl-257.local>
2016-11-21 13:54:38 -0700

    created the required init on card view

--- CardGame/View/CardView.swift
+++ CardGame/View/CardView.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class CardView
+@IBDesignable class CardView : UIView
 {
+    
     internal var frontImage : UIImageView
     internal var backImage : UIImageView
-    internal var isFaceUp: Bool
+    @IBInspectable internal var isFaceUp: Bool
+    @IBInspectable internal var cornerCurve : CGFloat
     internal var bottomLabel : UILabel
     internal var topLabel : UILabel
     
@@ -23,8 +25,25 @@ class CardView
         isFaceUp = Bool()
         bottomLabel = UILabel()
         topLabel = UILabel()
+        cornerCurve = CGFloat()
+        super.init(frame: CGRect(x:0,y:0,width:400, height:800))
     }
     
+    required init?(coder acoder:NSCoder)
+    {
+        frontImage = UIImageView()
+        backImage = UIImageView()
+        isFaceUp = Bool()
+        bottomLabel = UILabel()
+        topLabel = UILabel()
+        cornerCurve = CGFloat()
+        super.init(coder: acoder)
+    }
+    
+    func setup() -> Void
+    {
+        
+    }
     
     
 }
